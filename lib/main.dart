17f39c882968d535d0789a9f8b89b53c0bd7f8dd
Abderrahman <dakirallah.ar@gmail.com>
2021-04-24 18:11:13 +0000

    feature:Structured the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:dev_id_card/TextIconCard.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'TextIconCard.dart';
+import './pages/my_home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,108 +9,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String githubLink = 'black-purple/dev_id_card.git';
-  String email = 'example@example.com';
-  String phoneNum = '+212 645879000';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('images/avatar.png'),
-                  maxRadius: 80,
-                ),
-              ],
-            ),
-            SizedBox(
-              child: Divider(
-                thickness: 1,
-                color: Colors.white,
-              ),
-              height: 50,
-              width: 210,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //* Container1
-                TextIconCard(
-                  icon: Icons.phone,
-                  iconColor: Colors.green,
-                  fontSize: 20,
-                  cardText: phoneNum,
-                  afterIcon: 45,
-                ),
-                //* Container2
-                SizedBox(
-                  height: 20,
-                ),
-                TextIconCard(
-                  iconColor: Colors.amber,
-                  cardText: email,
-                  afterIcon: 20,
-                  icon: Icons.mail,
-                  fontSize: 20,
-                ),
-                //* Container3
-                SizedBox(
-                  height: 20,
-                ),
-                TextIconCard(
-                  iconColor: Color(0xFF443583),
-                  cardText: githubLink,
-                  afterIcon: 18,
-                  icon: FontAwesomeIcons.github,
-                  fontSize: 18,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Made with',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
